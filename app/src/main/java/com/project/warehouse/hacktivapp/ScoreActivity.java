a2c35e9826846e48e23aa1fdb5d4c890aba0651f
Eminarti Sianturi <eminarti.sianturi@happyfresh.com>
2022-02-17 20:57:20 +0700

    Implement onSaveInstance

--- app/src/main/java/com/project/warehouse/hacktivapp/ScoreActivity.java
+++ app/src/main/java/com/project/warehouse/hacktivapp/ScoreActivity.java
@@ -1,11 +1,11 @@
 package com.project.warehouse.hacktivapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.appcompat.widget.AppCompatTextView;
-
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.AppCompatTextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
@@ -30,6 +30,26 @@ public class ScoreActivity extends AppCompatActivity {
         scoreTeam2Label = findViewById(R.id.score_team2);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("STATE_SCORE_1", scoreTeam1);
+        outState.putInt("STATE_SCORE_2", scoreTeam2);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            scoreTeam1 = savedInstanceState.getInt("STATE_SCORE_1");
+            scoreTeam1Label.setText(String.valueOf(scoreTeam1));
+
+            scoreTeam2 = savedInstanceState.getInt("STATE_SCORE_2");
+            scoreTeam2Label.setText(String.valueOf(scoreTeam2));
+        }
+    }
+
     public void decreasePoint(View view) {
         if (view.getId() == R.id.button_decrease_team1) {
             scoreTeam1--;
@@ -48,5 +68,16 @@ public class ScoreActivity extends AppCompatActivity {
             scoreTeam2++;
             scoreTeam2Label.setText(String.valueOf(scoreTeam2));
         }
+    }
+
+    public void changeTheme(View view) {
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        recreate();
     }
 }
